9881006e858039ebf7c7f124c0067b42f054394d
nakamura <shop@goosys.sakura.ne.jp>
2024-07-18 05:38:09 +0000

    exec: bundle gem administrate-mistybird -t

---
+++ lib/administrate/mistybird.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "mistybird/version"
+
+module Administrate
+  module Mistybird
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/administrate/mistybird_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Administrate::Mistybird do
+  it "has a version number" do
+    expect(Administrate::Mistybird::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
