b5d27a9b60a35639516b33d56ea13acc36ccce4f
nakamura <shop@goosys.sakura.ne.jp>
2024-07-20 12:00:03 +0000

    feat: Add pass_parent_resource_to_nested_has_many patch

--- lib/administrate/mistybird.rb
+++ lib/administrate/mistybird.rb
@@ -4,6 +4,7 @@ require_relative "mistybird/version"
 require_relative "field/refile"
 require_relative "role_aware_resource_resolver"
 require_relative "patches/administrate_datetime_default"
+require_relative "patches/pass_parent_resource_to_nested_has_many"
 require_relative "patches/role_aware_resource_resolver"
 
 module Administrate
