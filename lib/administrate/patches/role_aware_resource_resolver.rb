f6af407a929e78b9ad74f0e2b7953df716ddb187
nakamura <shop@goosys.sakura.ne.jp>
2024-07-20 15:06:36 +0000

    fixup! feat: Add role-aware resource resolver to Administrate

--- lib/administrate/patches/role_aware_resource_resolver.rb
+++ lib/administrate/patches/role_aware_resource_resolver.rb
@@ -5,13 +5,11 @@ require "administrate/base_dashboard"
 
 Administrate::Page::Base.prepend (
   Module.new {
-    DASHBOARD_SUFFIX = "Dashboard".freeze
-
     # see https://github.com/thoughtbot/administrate/blob/35a21cfdc0d64b098a3f16543deb4739f80c1dbd/lib/administrate/page/base.rb#L9
     #     https://github.com/thoughtbot/administrate/blob/7cddab1b0c71beb8c78a294ad3d9659963efb859/lib/administrate/base_dashboard.rb#L24
     def resource_name
       @resource_name ||=
-        ActiveSupport::Inflector.demodulize(dashboard.class.to_s).chomp(DASHBOARD_SUFFIX).underscore
+        ActiveSupport::Inflector.demodulize(dashboard.class.to_s).chomp(self.class.const_get(:DASHBOARD_SUFFIX)).underscore
     end
   }
 )
@@ -22,7 +20,7 @@ Administrate::BaseDashboard.singleton_class.prepend (
 
     # see https://github.com/thoughtbot/administrate/blob/v1.0.0.beta1/lib/administrate/base_dashboard.rb#L24
     def model
-      ActiveSupport::Inflector.demodulize(to_s).chomp(DASHBOARD_SUFFIX).classify.constantize
+      ActiveSupport::Inflector.demodulize(to_s).chomp(self.class.const_get(:DASHBOARD_SUFFIX)).classify.constantize
     end
   }
 )
