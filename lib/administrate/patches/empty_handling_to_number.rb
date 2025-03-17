ce2ebd4818e37bf68a6ef7e741c0d188d230ea1a
nakamura <shop@goosys.sakura.ne.jp>
2025-03-17 11:36:36 +0000

    Implement empty handling for number fields and add corresponding tests

---
+++ lib/administrate/patches/empty_handling_to_number.rb
@@ -0,0 +1,13 @@
+require "administrate/field/number"
+
+Administrate::Field::HasMany.prepend (
+  Module.new {
+    # see https://github.com/thoughtbot/administrate/blob/13cf8fb4c588f8748108ca097723f98879a47227/lib/administrate/field/number.rb#L8
+    def to_s
+      return "-" if data.nil?
+      result = format_string % value
+      result = format(result) if options[:format]
+      prefix + result + suffix
+    end
+  }
+)
---
+++ spec/administrate/field/number_spec.rb
@@ -0,0 +1,20 @@
+require "rails_helper"
+require "administrate/field/number"
+
+describe Administrate::Field::Number do
+  describe "#to_s" do
+    context "when the data is nil" do
+      it "returns a dash" do
+        field = Administrate::Field::Number.new(:price, nil, :show)
+        expect(field.to_s).to eq("-")
+      end
+    end
+
+    context "when the data is not nil" do
+      it "returns the number as a string" do
+        field = Administrate::Field::Number.new(:price, 10_000, :show)
+        expect(field.to_s).to eq("10000")
+      end
+    end
+  end
+end
