337e33d32b319b31056b5737c150cdaf3cdbeb4a
nakamura <shop@goosys.sakura.ne.jp>
2025-03-18 10:19:23 +0000

    fixup! Implement empty handling for number fields and add corresponding tests

--- lib/administrate/patches/empty_handling_to_number.rb
+++ lib/administrate/patches/empty_handling_to_number.rb
@@ -1,6 +1,6 @@
 require "administrate/field/number"
 
-Administrate::Field::HasMany.prepend (
+Administrate::Field::Number.prepend (
   Module.new {
     # see https://github.com/thoughtbot/administrate/blob/13cf8fb4c588f8748108ca097723f98879a47227/lib/administrate/field/number.rb#L8
     def to_s
